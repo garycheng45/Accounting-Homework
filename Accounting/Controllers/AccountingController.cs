d1e544bbda598d9c0639aad32a9d6ddb7b468b51
garycheng45 <garycheng45@hotmail.com>
2017-06-16 22:14:08 +0800

    Accounting
    Accounting Layout and ViewModel Implement

---
+++ Accounting/Controllers/AccountingController.cs
@@ -0,0 +1,38 @@
+﻿using Accounting.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Accounting.Controllers
+{
+    public class AccountingController : Controller
+    {
+        // GET: Accounting
+        public ActionResult AccountIndex()
+        {
+			List<AccountViewModel> pageData = new List<AccountViewModel>();
+
+			for(int i = 1; i < 5; i++)
+			{
+				if(i == 3)
+				{
+					pageData.Add(
+					new AccountViewModel()
+					{ Amount = i * 30, Date = DateTime.Now, Type = AccountViewModel.E_Accou_Type.Income, Comment = "" }
+					);
+				}
+				else
+				{
+					pageData.Add(
+					new AccountViewModel()
+					{ Amount = i * 30, Date = DateTime.Now, Type = AccountViewModel.E_Accou_Type.OutCome, Comment = "" }
+					);
+				}				
+			}
+
+			return View(pageData);
+        }
+    }
+}
---
+++ Accounting/Models/ViewModel/AccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Accounting.Models.ViewModel
+{
+	public class AccountViewModel
+	{
+		public enum E_Accou_Type { Income, OutCome };
+		public E_Accou_Type Type { get; set; }
+		public int Amount {get; set;}
+		public DateTime Date { get; set; }
+		public string Comment { get; set; }
+	}
+}
